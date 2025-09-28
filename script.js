b82d6003ba9a8a8f7f7a45305aa9e4916590029b
abdur2711 <abdurrahman.ar89547@gmail.com>
2025-09-28 23:31:24 +0600

    Add bookmark sections functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const categoryContainer = document.getElementById('category-container');
 const newsContainer = document.getElementById('news-container');
+const bookmarkContainer = document.getElementById('bookmark-container');
+const bookmarkCount = document.getElementById('bookmark-count');
+
+let bookmarks = [];
 
 const loadCategory = () => {
   fetch('https://news-api-fs.vercel.app/api/categories')
@@ -45,21 +49,61 @@ const loadNewsByCategory = (id) => {
 }
 
 const showNewsByCategory = (articles) => {
-  console.log(articles)
-
   newsContainer.innerHTML = '';
 
   articles.forEach(article => {
     newsContainer.innerHTML += `
-      <div>
+      <div class="border border-gray-200 rounded-lg">
         <div>
           <img src="${article.image.srcset[5].url}">
         </div>
-        <h1>${article.title}</h1>
-        <p>${article.time}</p>
+        <div id="${article.id}" class="p-2">
+          <h1 class="font-extrabold">${article.title}</h1>
+          <p class="text-sm">${article.time}</p>
+          <button class="btn">Bookmark</button>
+        </div>
       </div>
     `
   })
+}
+
+// Add to bookmark
+newsContainer.addEventListener('click', (e) => {
+  if(e.target.innerText === 'Bookmark') {
+    handleBookmarks(e)
+  }
+})
+
+const handleBookmarks = (e) => {
+  const title = e.target.parentNode.children[0].innerText;
+    const id = e.target.parentNode.id;
+
+    bookmarks.push({
+      title: title,
+      id: id,
+    })
+    showBookmarks(bookmarks)
+}
+
+const showBookmarks = (bookmarks) => {
+  bookmarkContainer.innerHTML = '';
+
+  bookmarks.forEach(bookmark => {
+    bookmarkContainer.innerHTML += `
+      <div class="border border-gray-400 my-2 p-1">
+        <h1>${bookmark.title}</h1>
+        <button class="btn btn-xs" onclick="handleDeleteBookmark('${bookmark.id}')">Delete</button>
+      </div>
+    `
+  })
+  bookmarkCount.innerText = bookmarks.length;
+}
+
+const handleDeleteBookmark = (id) => {
+  const filteredBookmarks = bookmarks.filter(bookmark => bookmark.id !== id)
+
+  bookmarks = filteredBookmarks;
+  showBookmarks(bookmarks);
 }
 
 loadCategory();
