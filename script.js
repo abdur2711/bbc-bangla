1bc28cc475bf51afdf26a753b2c3c8767f9405b4
abdur2711 <abdurrahman.ar89547@gmail.com>
2025-09-28 08:25:32 +0600

    Add border bottom when clicked on category

--- script.js
+++ script.js
@@ -15,9 +15,19 @@ const loadCategory = () => {
 const showCategory = (categories) => {
   categories.forEach(category => {
     categoryContainer.innerHTML += `
-    <li id="${category.id}" class="hover:border-b-4 hover:border-red-600 cursor-pointer">${category.title}</li>
+    <li id="${category.id}" class="hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${category.title}</li>
     `
   });
+  categoryContainer.addEventListener('click', (e) => {
+    const allLi = document.querySelectorAll('li');
+    allLi.forEach(li => {
+      li.classList.remove('border-b-4')
+    })
+
+    if (e.target.localName === 'li') {
+      e.target.classList.add('border-b-4')
+    }
+  })
 }
 
 
